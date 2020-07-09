7f904854cf7b34251592e66eeb0e84c4db633365
Steven Batcup <steve.batcup@gmail.com>
2020-07-09 14:02:10 +0100

    removed web folder, added ios folder, renamed svg

---
+++ ios/build/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
@@ -0,0 +1,20 @@
+dependencies: \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Modules/module.modulemap \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Pods/Firebase/CoreOnly/Sources/module.modulemap \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/Flutter/Flutter.framework/Headers/Flutter.h
---
+++ ios/build/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/armv7/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/steve/Code/Portfolio\ 2020/booklogr/ios/build/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
